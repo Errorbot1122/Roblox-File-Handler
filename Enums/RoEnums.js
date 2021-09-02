50ebe001b5b9e88ceb267312866739819955cb99
Errorbot1122 <65428515+ErrorBot1122@users.noreply.github.com>
2021-09-02 04:18:19 +0000

    📝 — "Optimized" 'RoEnums.js' for "ease-of-use"

--- Enums/RoEnums.js
+++ Enums/RoEnums.js
@@ -1,6 +1,30 @@
-module.exports = {
-  Material: require('./Material.js'),
-  SurfaceType: require('./SurfaceType.js'),
-  InputType: require('./InputType.js'),
-  PartType: require('./PartType.js')
-}
+// Import the filesystem module
+const fs = require("fs");
+
+const directory_name = "Enums";
+
+let exportRequired = {}
+
+// Function to get current filepaths
+// in directory
+let filepaths = fs.readdirSync(directory_name);
+  
+
+filepaths.forEach((filepath) => {
+
+  let fileExtentionIndex = filepath.indexOf(".")
+
+  if (fileExtentionIndex != -1) {
+
+    let extention = filepath.slice(fileExtentionIndex)
+    let fileName = filepath.slice(0, fileExtentionIndex)
+
+    if (extention == ".js" && fileName != "RoEnums") {
+      
+      exportRequired[fileName] = require(`./${filepath}`)
+    }
+  }
+  
+});
+
+module.exports = exportRequired
